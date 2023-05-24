f66c842507dfc3af97edc7947529169fba75a3fc
Joey <qpengf@gmail.com>
2023-05-24 12:33:24 +0800

    Create getCookie.js

---
+++ getCookie.js
@@ -0,0 +1,12 @@
+// 获取 CSRF token
+if (typeof csrfToken === 'undefined') {
+    let csrfToken;
+    let metaTags = document.getElementsByTagName('meta');
+    for (let i = 0; i < metaTags.length; i++) {
+        if (metaTags[i].getAttribute('name') === 'csrf-token') {
+            csrfToken = metaTags[i].getAttribute('content');
+            break;
+        }
+    }
+}
+console.log("CSRF Token: ", csrfToken);
